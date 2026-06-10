552dddd92568e5bff77e7aedcdfcf22434948135
mfirhas <fathir.irhas@gmail.com>
2026-06-10 21:40:51 +0700

    Add find source

--- src/lib.rs
+++ src/lib.rs
@@ -349,4 +349,12 @@ where
     {
         self.chain().find_map(|e| e.downcast_ref::<T>())
     }
+
+    #[inline]
+    pub fn find_source<T>(&self) -> Option<&T>
+    where
+        T: Error + 'static,
+    {
+        self.chain().find_map(|err| err.downcast_ref::<T>())
+    }
 }
