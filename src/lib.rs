37e845aedfe70360a515c4ef852dfa54300047d4
mfirhas <fathir.irhas@gmail.com>
2026-06-12 20:52:22 +0700

    Rename extension methods name

--- src/lib.rs
+++ src/lib.rs
@@ -344,14 +344,14 @@ impl<ID: Debug, P: Prefix, D: Debug> Error for GErr<ID, P, D> {
 pub trait ResultExt<T> {
     #[must_use]
     #[track_caller]
-    fn gerr<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
+    fn context<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
     where
         ID: Id,
         P: Prefix;
 
     #[must_use]
     #[track_caller]
-    fn gerr_with<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
+    fn with_context<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
     where
         ID: Id,
         P: Prefix,
@@ -360,7 +360,7 @@ pub trait ResultExt<T> {
 
     #[must_use]
     #[track_caller]
-    fn wrap<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D>;
+    fn wrap_err<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D>;
 }
 
 impl<T, E> ResultExt<T> for core::result::Result<T, E>
@@ -368,6 +368,68 @@ where
     E: Error + Send + Sync + 'static,
 {
     #[track_caller]
+    fn context<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
+    where
+        ID: Id,
+        P: Prefix,
+    {
+        let message = message.into();
+        let location = Location::caller();
+
+        self.map_err(|source| {
+            GErr::<ID, P, ()>::new_untracked(message, location).set_source(source)
+        })
+    }
+
+    #[track_caller]
+    fn with_context<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
+    where
+        ID: Id,
+        P: Prefix,
+        F: FnOnce() -> M,
+        M: Into<Cow<'static, str>>,
+    {
+        let message = func().into();
+        let location = Location::caller();
+
+        self.map_err(|source| {
+            GErr::<ID, P, ()>::new_untracked(message, location).set_source(source)
+        })
+    }
+
+    #[track_caller]
+    fn wrap_err<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D> {
+        self.map_err(|source| err.set_source(source))
+    }
+}
+
+pub trait GResultExt<T> {
+    #[must_use]
+    #[track_caller]
+    fn gerr<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
+    where
+        ID: Id,
+        P: Prefix;
+
+    #[must_use]
+    #[track_caller]
+    fn with_gerr<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
+    where
+        ID: Id,
+        P: Prefix,
+        F: FnOnce() -> M,
+        M: Into<Cow<'static, str>>;
+
+    #[must_use]
+    #[track_caller]
+    fn wrap_gerr<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D>;
+}
+
+impl<T, E> GResultExt<T> for core::result::Result<T, E>
+where
+    E: Into<GErrSource> + Error + Send + Sync + 'static,
+{
+    #[track_caller]
     fn gerr<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
     where
         ID: Id,
@@ -377,74 +439,12 @@ where
         let location = Location::caller();
 
         self.map_err(|source| {
-            GErr::<ID, P, ()>::new_untracked(message, location).set_source(source)
-        })
-    }
-
-    #[track_caller]
-    fn gerr_with<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
-    where
-        ID: Id,
-        P: Prefix,
-        F: FnOnce() -> M,
-        M: Into<Cow<'static, str>>,
-    {
-        let message = func().into();
-        let location = Location::caller();
-
-        self.map_err(|source| {
-            GErr::<ID, P, ()>::new_untracked(message, location).set_source(source)
-        })
-    }
-
-    #[track_caller]
-    fn wrap<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D> {
-        self.map_err(|source| err.set_source(source))
-    }
-}
-
-pub trait GResultExt<T> {
-    #[must_use]
-    #[track_caller]
-    fn gerrsource<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
-    where
-        ID: Id,
-        P: Prefix;
-
-    #[must_use]
-    #[track_caller]
-    fn gerrsource_with<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
-    where
-        ID: Id,
-        P: Prefix,
-        F: FnOnce() -> M,
-        M: Into<Cow<'static, str>>;
-
-    #[must_use]
-    #[track_caller]
-    fn wrap_gerrsource<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D>;
-}
-
-impl<T, E> GResultExt<T> for core::result::Result<T, E>
-where
-    E: Into<GErrSource> + Error + Send + Sync + 'static,
-{
-    #[track_caller]
-    fn gerrsource<ID, P>(self, message: impl Into<Cow<'static, str>>) -> Result<T, ID, P>
-    where
-        ID: Id,
-        P: Prefix,
-    {
-        let message = message.into();
-        let location = Location::caller();
-
-        self.map_err(|source| {
             GErr::<ID, P, ()>::new_untracked(message, location).set_source_gerr(source)
         })
     }
 
     #[track_caller]
-    fn gerrsource_with<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
+    fn with_gerr<ID, P, F, M>(self, func: F) -> Result<T, ID, P>
     where
         ID: Id,
         P: Prefix,
@@ -460,7 +460,7 @@ where
     }
 
     #[track_caller]
-    fn wrap_gerrsource<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D> {
+    fn wrap_gerr<ID, P: Prefix, D>(self, err: GErr<ID, P, D>) -> Result<T, ID, P, D> {
         self.map_err(|source| err.set_source_gerr(source))
     }
 }
