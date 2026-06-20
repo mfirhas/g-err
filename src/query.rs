98e1ab286979fd089626480a1fec7fe606d72bbe
mfirhas <fathir.irhas@gmail.com>
2026-06-20 21:48:29 +0700

    Add query multiple ids, data, and sources

--- src/query.rs
+++ src/query.rs
@@ -26,11 +26,11 @@ where
     }
 
     #[inline]
-    pub fn find_id<T>(&self) -> Option<&T>
+    pub fn ids<T>(&self) -> impl Iterator<Item = &T>
     where
         T: Any,
     {
-        self.iter().find_map(|item| match item {
+        self.iter().filter_map(|item| match item {
             IterItem::Root(gerr) => (gerr.id() as &dyn Any).downcast_ref::<T>(),
 
             IterItem::GErr(gerr) => (&*gerr.id as &dyn Any).downcast_ref::<T>(),
@@ -40,11 +40,11 @@ where
     }
 
     #[inline]
-    pub fn find_data<T>(&self) -> Option<&T>
+    pub fn datas<T>(&self) -> impl Iterator<Item = &T>
     where
         T: Any,
     {
-        self.iter().find_map(|item| match item {
+        self.iter().filter_map(|item| match item {
             IterItem::Root(gerr) => gerr
                 .data()
                 .and_then(|data| (data as &dyn Any).downcast_ref::<T>()),
@@ -59,16 +59,40 @@ where
     }
 
     #[inline]
-    pub fn find_source<E>(&self) -> Option<&E>
+    pub fn sources_of<E>(&self) -> impl Iterator<Item = &E>
     where
         E: Error + 'static,
     {
-        self.iter().find_map(|item| match item {
+        self.iter().filter_map(|item| match item {
             IterItem::Err(err) => err.downcast_ref::<E>(),
 
             IterItem::GErr(gerr) => (gerr as &dyn Error).downcast_ref::<E>(),
 
             IterItem::Root(_) => None,
         })
+    }
+
+    #[inline]
+    pub fn find_id<T>(&self) -> Option<&T>
+    where
+        T: Any,
+    {
+        self.ids::<T>().next()
+    }
+
+    #[inline]
+    pub fn find_data<T>(&self) -> Option<&T>
+    where
+        T: Any,
+    {
+        self.datas::<T>().next()
+    }
+
+    #[inline]
+    pub fn find_source<E>(&self) -> Option<&E>
+    where
+        E: Error + 'static,
+    {
+        self.sources_of::<E>().next()
     }
 }
