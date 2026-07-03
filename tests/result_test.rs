cc9131ff60939d3795c9e0efd589ef3c75380fb0
mfirhas <fathir.irhas@gmail.com>
2026-07-03 15:28:10 +0700

    Init result tests

---
+++ tests/result_test.rs
@@ -0,0 +1,19 @@
+use g_err::*;
+
+#[test]
+fn test_result_ext_manual() {}
+
+#[test]
+fn test_result_ext_auto() {}
+
+#[test]
+fn test_gresult_ext_manual() {}
+
+#[test]
+fn test_gresult_ext_auto() {}
+
+#[test]
+fn test_box_gresult_ext_manual() {}
+
+#[test]
+fn test_box_gresult_ext_auto() {}
