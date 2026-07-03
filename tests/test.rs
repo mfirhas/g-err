2803177290ad4b1d6b3a6933f5353090f8225c75
mfirhas <fathir.irhas@gmail.com>
2026-07-03 15:24:02 +0700

    Init result tests

--- tests/test.rs
+++ tests/test.rs
@@ -47,6 +47,9 @@ mod sources_test;
 #[path = "tags_test.rs"]
 mod tags_test;
 
+#[path = "result_test.rs"]
+mod result_test;
+
 fn parse_age(str_age: &str) -> Result<i32, NoID, PrefixB> {
     str_age.parse().context_auto("string age parsing")
 }
