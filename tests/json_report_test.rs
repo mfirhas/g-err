9f4ac5cc97bba9c151984c43c01993104ef3eb44
mfirhas <fathir.irhas@gmail.com>
2026-07-09 11:35:50 +0700

    Add json data test

--- tests/json_report_test.rs
+++ tests/json_report_test.rs
@@ -137,3 +137,192 @@ fn test_display_json_data() {
     let display_data_str = ::serde_json::to_string_pretty(&display_data).unwrap();
     assert_eq!(display_data_str, EXPECTED_DISPLAY_JSON_DATA_DEBUG);
 }
+
+const EXPECTED_JSON_DATA_DEBUG: &str = r#"{
+  "id": "AutoID",
+  "prefix": "AutoPrefix",
+  "message": "pretty error: l2k3mr2l3r",
+  "tags": [
+    "tag1",
+    "tag2",
+    "tag3"
+  ],
+  "data": {
+    "user_id": 123,
+    "user_name": "ajo"
+  },
+  "location": {
+    "file": "tests/json_report_test.rs",
+    "line": 297,
+    "column": 48
+  },
+  "sources": [
+    {
+      "id": null,
+      "prefix": null,
+      "message": "invalid digit found in string",
+      "tags": null,
+      "data": null,
+      "location": null,
+      "sources": null,
+      "help": null
+    },
+    {
+      "id": 40,
+      "prefix": "[400][NOT_FOUND]",
+      "message": "input is invalid: qwe",
+      "tags": [
+        "bad_request",
+        "invalid_input"
+      ],
+      "data": [
+        "user_name",
+        "ajo"
+      ],
+      "location": {
+        "file": "tests/json_report_test.rs",
+        "line": 308,
+        "column": 14
+      },
+      "sources": [
+        {
+          "id": null,
+          "prefix": null,
+          "message": "invalid digit found in string",
+          "tags": null,
+          "data": null,
+          "location": null,
+          "sources": null,
+          "help": null
+        },
+        {
+          "id": null,
+          "prefix": "[OUTBOUND]",
+          "message": "upstream error",
+          "tags": null,
+          "data": null,
+          "location": {
+            "file": "tests/json_report_test.rs",
+            "line": 317,
+            "column": 18
+          },
+          "sources": [
+            {
+              "id": null,
+              "prefix": null,
+              "message": "got error from user service",
+              "tags": null,
+              "data": [
+                "caused by:",
+                "timout"
+              ],
+              "location": {
+                "file": "tests/json_report_test.rs",
+                "line": 317,
+                "column": 68
+              },
+              "sources": null,
+              "help": "contact user service steward"
+            }
+          ],
+          "help": null
+        }
+      ],
+      "help": "pass valid input"
+    },
+    {
+      "id": "AutoID",
+      "prefix": null,
+      "message": "timeout checks",
+      "tags": [
+        "user_service",
+        "timeout"
+      ],
+      "data": null,
+      "location": {
+        "file": "tests/json_report_test.rs",
+        "line": 319,
+        "column": 14
+      },
+      "sources": [
+        {
+          "id": null,
+          "prefix": null,
+          "message": "too many open files",
+          "tags": [
+            "tmof"
+          ],
+          "data": [
+            "MAX",
+            50000
+          ],
+          "location": {
+            "file": "tests/json_report_test.rs",
+            "line": 322,
+            "column": 18
+          },
+          "sources": null,
+          "help": null
+        }
+      ],
+      "help": null
+    },
+    {
+      "id": null,
+      "prefix": null,
+      "message": "connection timeout",
+      "tags": null,
+      "data": null,
+      "location": {
+        "file": "tests/json_report_test.rs",
+        "line": 323,
+        "column": 14
+      },
+      "sources": null,
+      "help": null
+    }
+  ],
+  "help": "send valid request",
+  "backtrace": "<disabled>"
+}"#;
+
+#[cfg(feature = "serde")]
+#[test]
+fn test_json_data() {
+    let user_id = 123;
+    let user_name = "ajo".into();
+    let req_id = "l2k3mr2l3r";
+    let input = "qwe";
+    let input_err = input.parse::<u64>().unwrap_err();
+    let gerr: GErr<AutoID, AutoPrefix, Data> = gerr!("pretty error: {req_id}";
+        id_auto,
+        prefix_auto,
+        tag="tag1",
+        tags=["tag2", "tag3"],
+        data= Data {
+            user_id,
+            user_name,
+        },
+        help="send valid request",
+        source=input_err.clone(),
+        gerr=gerr!("input is invalid: {}", input;
+            id=40,
+            prefix="[400]",
+            tag="bad_request",
+            tag="invalid_input",
+            help="pass valid input",
+            aprefix = "[NOT_FOUND]",
+            data=("user_name".to_string(), "ajo".to_string()),
+            source = input_err,
+            gerr=gerr!("upstream error"; prefix="[OUTBOUND]", gerr=gerr!("got error from user service"; data=("caused by:".to_string(), "timout".to_string()), help="contact user service steward")),
+        ),
+        gerr=gerr!("timeout checks";
+            id_auto=AutoID,
+            tags=["user_service", "timeout"],
+            gerr=gerr!("too many open files"; tag="tmof", data=("MAX", 50000))),
+        gerr=gerr!("connection timeout"; id_auto=NoID, data=NoData),
+    );
+
+    let json_data = serde_json::to_string_pretty(&gerr.json_data()).unwrap();
+    assert_eq!(json_data, EXPECTED_JSON_DATA_DEBUG);
+}
