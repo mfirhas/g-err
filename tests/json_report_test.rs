04d4c710aaefe501dbdbf5d88a232faac8986692
mfirhas <fathir.irhas@gmail.com>
2026-07-16 17:25:30 +0700

    Move test data

--- tests/json_report_test.rs
+++ tests/json_report_test.rs
@@ -5,10 +5,10 @@ mod setup_test;
 use setup_test::*;
 
 #[cfg(feature = "serde")]
-use g_err::*;
+use g_err::{json::JsonReport, *};
 
-#[cfg(feature = "serde")]
-use g_err::json::JsonReport;
+#[path = "json_report_test_data.rs"]
+mod json_report_test_data;
 
 #[cfg(feature = "serde")]
 #[test]
@@ -51,58 +51,9 @@ fn test_json_report() {
 }
 
 #[cfg(feature = "serde")]
-const EXPECTED_DISPLAY_JSON_DATA_DEBUG: &str = r#"{
-  "id": "AutoID",
-  "code": "AutoCode",
-  "message": "pretty error: l2k3mr2l3r",
-  "tags": [
-    "tag1",
-    "tag2",
-    "tag3"
-  ],
-  "data": {
-    "user_id": 123,
-    "user_name": "ajo"
-  },
-  "help": "send valid request",
-  "causes": [
-    {
-      "message": "invalid digit found in string",
-      "caused_by": null
-    },
-    {
-      "message": "400 - input is invalid: qwe",
-      "caused_by": [
-        {
-          "message": "invalid digit found in string",
-          "caused_by": null
-        },
-        {
-          "message": "[OUTBOUND] - upstream error",
-          "caused_by": [
-            {
-              "message": "got error from user service",
-              "caused_by": null
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "message": "timeout checks",
-      "caused_by": [
-        {
-          "message": "too many open files",
-          "caused_by": null
-        }
-      ]
-    }
-  ]
-}"#;
-
-#[cfg(feature = "serde")]
 #[test]
 fn test_display_json_data() {
+    use json_report_test_data::test_display_json_data_data;
     let user_id = 123;
     let user_name = "ajo".into();
     let req_id = "l2k3mr2l3r";
@@ -137,161 +88,16 @@ fn test_display_json_data() {
 
     let display_data = gerr.display_json_data();
     let display_data_str = ::serde_json::to_string_pretty(&display_data).unwrap();
-    assert_eq!(display_data_str, EXPECTED_DISPLAY_JSON_DATA_DEBUG);
+    assert_eq!(
+        display_data_str,
+        test_display_json_data_data::EXPECTED_DISPLAY_JSON_DATA_DEBUG
+    );
 }
-
-#[cfg(feature = "serde")]
-const EXPECTED_JSON_DATA_DEBUG: &str = r#"{
-  "id": "AutoID",
-  "code": "AutoCode",
-  "message": "pretty error: l2k3mr2l3r",
-  "tags": [
-    "tag1",
-    "tag2",
-    "tag3"
-  ],
-  "data": {
-    "user_id": 123,
-    "user_name": "ajo"
-  },
-  "location": {
-    "file": "tests/json_report_test.rs",
-    "line": 300,
-    "column": 43
-  },
-  "sources": [
-    {
-      "id": null,
-      "code": null,
-      "message": "invalid digit found in string",
-      "tags": null,
-      "data": null,
-      "location": null,
-      "sources": null,
-      "help": null
-    },
-    {
-      "id": 40,
-      "code": "400",
-      "message": "input is invalid: qwe",
-      "tags": [
-        "bad_request",
-        "invalid_input"
-      ],
-      "data": [
-        "user_name",
-        "ajo"
-      ],
-      "location": {
-        "file": "tests/json_report_test.rs",
-        "line": 310,
-        "column": 14
-      },
-      "sources": [
-        {
-          "id": null,
-          "code": null,
-          "message": "invalid digit found in string",
-          "tags": null,
-          "data": null,
-          "location": null,
-          "sources": null,
-          "help": null
-        },
-        {
-          "id": null,
-          "code": "[OUTBOUND]",
-          "message": "upstream error",
-          "tags": null,
-          "data": null,
-          "location": {
-            "file": "tests/json_report_test.rs",
-            "line": 319,
-            "column": 18
-          },
-          "sources": [
-            {
-              "id": null,
-              "code": null,
-              "message": "got error from user service",
-              "tags": null,
-              "data": [
-                "caused by:",
-                "timout"
-              ],
-              "location": {
-                "file": "tests/json_report_test.rs",
-                "line": 319,
-                "column": 66
-              },
-              "sources": null,
-              "help": "contact user service steward"
-            }
-          ],
-          "help": null
-        }
-      ],
-      "help": "pass valid input"
-    },
-    {
-      "id": "AutoID",
-      "code": null,
-      "message": "timeout checks",
-      "tags": [
-        "user_service",
-        "timeout"
-      ],
-      "data": null,
-      "location": {
-        "file": "tests/json_report_test.rs",
-        "line": 321,
-        "column": 14
-      },
-      "sources": [
-        {
-          "id": null,
-          "code": null,
-          "message": "too many open files",
-          "tags": [
-            "tmof"
-          ],
-          "data": [
-            "MAX",
-            50000
-          ],
-          "location": {
-            "file": "tests/json_report_test.rs",
-            "line": 324,
-            "column": 18
-          },
-          "sources": null,
-          "help": null
-        }
-      ],
-      "help": null
-    },
-    {
-      "id": "AutoID",
-      "code": null,
-      "message": "connection timeout",
-      "tags": null,
-      "data": null,
-      "location": {
-        "file": "tests/json_report_test.rs",
-        "line": 325,
-        "column": 14
-      },
-      "sources": null,
-      "help": null
-    }
-  ],
-  "help": "send valid request",
-  "backtrace": "<disabled>"
-}"#;
 
 #[cfg(feature = "serde")]
 #[test]
 fn test_json_data() {
+    use json_report_test_data::test_json_data_data;
     let user_id = 123;
     let user_name = "ajo".into();
     let req_id = "l2k3mr2l3r";
@@ -326,5 +132,5 @@ fn test_json_data() {
     );
 
     let json_data = serde_json::to_string_pretty(&gerr.json_data()).unwrap();
-    assert_eq!(json_data, EXPECTED_JSON_DATA_DEBUG);
+    assert_eq!(json_data, test_json_data_data::EXPECTED_JSON_DATA_DEBUG);
 }
---
+++ tests/json_report_test_data.rs
@@ -0,0 +1,202 @@
+pub mod test_display_json_data_data {
+    #[cfg(feature = "serde")]
+    pub const EXPECTED_DISPLAY_JSON_DATA_DEBUG: &str = r#"{
+  "id": "AutoID",
+  "code": "AutoCode",
+  "message": "pretty error: l2k3mr2l3r",
+  "tags": [
+    "tag1",
+    "tag2",
+    "tag3"
+  ],
+  "data": {
+    "user_id": 123,
+    "user_name": "ajo"
+  },
+  "help": "send valid request",
+  "causes": [
+    {
+      "message": "invalid digit found in string",
+      "caused_by": null
+    },
+    {
+      "message": "400 - input is invalid: qwe",
+      "caused_by": [
+        {
+          "message": "invalid digit found in string",
+          "caused_by": null
+        },
+        {
+          "message": "[OUTBOUND] - upstream error",
+          "caused_by": [
+            {
+              "message": "got error from user service",
+              "caused_by": null
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "message": "timeout checks",
+      "caused_by": [
+        {
+          "message": "too many open files",
+          "caused_by": null
+        }
+      ]
+    }
+  ]
+}"#;
+}
+
+pub mod test_json_data_data {
+    #[cfg(feature = "serde")]
+    pub const EXPECTED_JSON_DATA_DEBUG: &str = r#"{
+  "id": "AutoID",
+  "code": "AutoCode",
+  "message": "pretty error: l2k3mr2l3r",
+  "tags": [
+    "tag1",
+    "tag2",
+    "tag3"
+  ],
+  "data": {
+    "user_id": 123,
+    "user_name": "ajo"
+  },
+  "location": {
+    "file": "tests/json_report_test.rs",
+    "line": 106,
+    "column": 43
+  },
+  "sources": [
+    {
+      "id": null,
+      "code": null,
+      "message": "invalid digit found in string",
+      "tags": null,
+      "data": null,
+      "location": null,
+      "sources": null,
+      "help": null
+    },
+    {
+      "id": 40,
+      "code": "400",
+      "message": "input is invalid: qwe",
+      "tags": [
+        "bad_request",
+        "invalid_input"
+      ],
+      "data": [
+        "user_name",
+        "ajo"
+      ],
+      "location": {
+        "file": "tests/json_report_test.rs",
+        "line": 116,
+        "column": 14
+      },
+      "sources": [
+        {
+          "id": null,
+          "code": null,
+          "message": "invalid digit found in string",
+          "tags": null,
+          "data": null,
+          "location": null,
+          "sources": null,
+          "help": null
+        },
+        {
+          "id": null,
+          "code": "[OUTBOUND]",
+          "message": "upstream error",
+          "tags": null,
+          "data": null,
+          "location": {
+            "file": "tests/json_report_test.rs",
+            "line": 125,
+            "column": 18
+          },
+          "sources": [
+            {
+              "id": null,
+              "code": null,
+              "message": "got error from user service",
+              "tags": null,
+              "data": [
+                "caused by:",
+                "timout"
+              ],
+              "location": {
+                "file": "tests/json_report_test.rs",
+                "line": 125,
+                "column": 66
+              },
+              "sources": null,
+              "help": "contact user service steward"
+            }
+          ],
+          "help": null
+        }
+      ],
+      "help": "pass valid input"
+    },
+    {
+      "id": "AutoID",
+      "code": null,
+      "message": "timeout checks",
+      "tags": [
+        "user_service",
+        "timeout"
+      ],
+      "data": null,
+      "location": {
+        "file": "tests/json_report_test.rs",
+        "line": 127,
+        "column": 14
+      },
+      "sources": [
+        {
+          "id": null,
+          "code": null,
+          "message": "too many open files",
+          "tags": [
+            "tmof"
+          ],
+          "data": [
+            "MAX",
+            50000
+          ],
+          "location": {
+            "file": "tests/json_report_test.rs",
+            "line": 130,
+            "column": 18
+          },
+          "sources": null,
+          "help": null
+        }
+      ],
+      "help": null
+    },
+    {
+      "id": "AutoID",
+      "code": null,
+      "message": "connection timeout",
+      "tags": null,
+      "data": null,
+      "location": {
+        "file": "tests/json_report_test.rs",
+        "line": 131,
+        "column": 14
+      },
+      "sources": null,
+      "help": null
+    }
+  ],
+  "help": "send valid request",
+  "backtrace": "<disabled>"
+}"#;
+}
